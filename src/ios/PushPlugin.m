3e412ddab46254698fb2fa7f6804ba4f14f4135f
Piyakorn Pete <piyakorn@satisbuy.com>
2014-11-18 15:34:25 +0700

    debug 2

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -220,10 +220,10 @@
         [self successWithMessage:[NSString stringWithFormat:@"%@", jsonStr]];
         
 
-        NSString * jsCallBack2 = [NSString stringWithFormat:@"setTimeout(function(){alert(%@(%@))}, 0);", self.callback, jsonStr];
+        NSString * jsCallBack2 = [NSString stringWithFormat:@"setTimeout(function(){alert('%@(%@)');}, 0);", self.callback, jsonStr];
         [self.webView stringByEvaluatingJavaScriptFromString:jsCallBack2];
 
-        NSString * jsCallBack = [NSString stringWithFormat:@"setTimeout(function(){%@(%@)}, 0);", self.callback, jsonStr];
+        NSString * jsCallBack = [NSString stringWithFormat:@"setTimeout(function(){%@(%@);}, 0);", self.callback, jsonStr];
         [self.webView stringByEvaluatingJavaScriptFromString:jsCallBack];
         
         self.notificationMessage = nil;
